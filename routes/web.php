e0d204906603e62a592d0b320658b7d4a9fb3a3e
Seyedmoslem Mahdavi <95871652+Mahdavi3649@users.noreply.github.com>
2022-06-21 15:06:08 +0200

    created database & Route resources group

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')->prefix('admin')->namespace('Admin')->name('admin.')->group(function () {
+    Route::get('/', 'HomeController@index')->name('dashboard'); // /admin.dashboard
+    
+});
